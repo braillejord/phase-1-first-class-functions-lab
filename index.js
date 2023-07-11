a5331520dcd8d9d3897e2d44b618131f24267c93
BreElle Wells <breellejordyn@gmail.com>
2023-07-11 13:18:21 -0600

    complete 7 tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driver) {
+    return driver.slice(0,2)
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = function(driver) {
+    return driver.slice(2, 4)
+}
+
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function() {
+        return (fare * fare)
+    }
+}
